a2c4e27c77c9b402144cc79b4af9f2ce814266c8
Sillamol shibu <sillamolshibu@gmail.com>
2026-08-29 19:04:48 +0530

    Update faculty.js

--- src/data/faculty.js
+++ src/data/faculty.js
@@ -1,45 +1,65 @@
 // Faculty Data for St. Berchmans College MCA Department
-// Add real faculty information here as the department updates
+// Department of Computer Applications (MCA) - Faculty Directory
 
 export const facultyMembers = [
   {
-    id: "fac-hod",
-    name: "Smitha",
-    designation: "Head of Department",
+    id: "fac-1",
+    name: "Ms. Smitha Krishnan",
+    title: "Ms.",
+    designation: "Assistant Professor",
+    role: "Head of the Department (HOD)",
     isHOD: true,
-    isPlaceholder: false,
-    photo: null, // User can place image in src/assets/images/faculty/
-    email: "",
-    qualification: ""
+    isTutor: false,
+    department: "Computer Science",
+    qualification: "MCA",
+    email: "Ksmitha21@rediffmail.com",
+    photo: "/images/faculty/smitha-krishnan.jpg",
+    photoFilename: "smitha-krishnan.jpg",
+    isPlaceholder: false
   },
   {
-    id: "fac-ph-1",
-    name: "Faculty Member",
-    designation: "Department Faculty",
+    id: "fac-2",
+    name: "Ms. Rini Elizabeth Jacob",
+    title: "Ms.",
+    designation: "Assistant Professor",
+    role: "Tutor",
     isHOD: false,
-    isPlaceholder: true,
-    photo: null,
-    email: "",
-    qualification: ""
+    isTutor: true,
+    department: "Computer Science",
+    qualification: "MCA",
+    email: "riniarun84@gmail.com",
+    photo: "/images/faculty/rini-elizabeth-jacob.jpg",
+    photoFilename: "rini-elizabeth-jacob.jpg",
+    isPlaceholder: false
   },
   {
-    id: "fac-ph-2",
-    name: "Faculty Member",
-    designation: "Department Faculty",
+    id: "fac-3",
+    name: "Ms. Aswathy Manoharan",
+    title: "Ms.",
+    designation: "Assistant Professor",
+    role: null,
     isHOD: false,
-    isPlaceholder: true,
-    photo: null,
-    email: "",
-    qualification: ""
+    isTutor: false,
+    department: "Computer Science",
+    qualification: "MCA",
+    email: "aswathymn@localhost",
+    photo: "/images/faculty/aswathy-manoharan.jpg",
+    photoFilename: "aswathy-manoharan.jpg",
+    isPlaceholder: false
   },
   {
-    id: "fac-ph-3",
-    name: "Faculty Member",
-    designation: "Department Faculty",
+    id: "fac-4",
+    name: "Mr. Sachin Varghese Biju",
+    title: "Mr.",
+    designation: "Assistant Professor",
+    role: null,
     isHOD: false,
-    isPlaceholder: true,
-    photo: null,
-    email: "",
-    qualification: ""
+    isTutor: false,
+    department: "Computer Science",
+    qualification: "M.Sc. Statistics (Applied)",
+    email: "sachinbiju746@gmail.com",
+    photo: "/images/faculty/sachin-varghese-biju.jpg",
+    photoFilename: "sachin-varghese-biju.jpg",
+    isPlaceholder: false
   }
 ];
