aa24d883e9a8cabacc56e3008e56e1826a23e803
akshitha-akhilesh <akshithaakhilesh02@gmail.com>
2026-08-31 23:58:54 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -75,7 +75,7 @@ export default function Home() {
               <span className="w-10 h-px bg-[#c9784d]" />
 
               <span className="text-[10px] font-semibold uppercase tracking-[0.24em] text-stone-500">
-                About the MCA Programme
+                Abo the MCA Programme
               </span>
             </div>
 
